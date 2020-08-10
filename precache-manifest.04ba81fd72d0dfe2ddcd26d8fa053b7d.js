4025cea6a7ebea4c90ec3a194d0f4177788fcc8a
shuaigechen <707626760@qq.com>
2020-08-10 19:47:39 +0800

    deploy

---
+++ precache-manifest.04ba81fd72d0dfe2ddcd26d8fa053b7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1ecb3c26e8e56e4d081",
+    "url": "/money-1-website/css/app.a8226386.css"
+  },
+  {
+    "revision": "328bd562a8ae11429783e28f01584f04",
+    "url": "/money-1-website/index.html"
+  },
+  {
+    "revision": "f1ecb3c26e8e56e4d081",
+    "url": "/money-1-website/js/app.3a33950c.js"
+  },
+  {
+    "revision": "40111c338c16ec968404",
+    "url": "/money-1-website/js/chunk-vendors.c9f9a11f.js"
+  },
+  {
+    "revision": "b0c72820654d086f2b526db8c6a20387",
+    "url": "/money-1-website/manifest.json"
+  },
+  {
+    "revision": "0eaededbee0dc2835ac25d55b4c19552",
+    "url": "/money-1-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-1-website/robots.txt"
+  }
+]);
